51fe678f10af3bf3946bbb3925810681db096b05
Jacob Wheeler <115048992+JacobWheel-er@users.noreply.github.com>
2022-10-06 20:30:24 -0400

    Create letter.cpp

---
+++ letter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "My main goal by the end of this course is to have the knowledge and ability to apply all I know of C++ to be able to provide for myself and my fiancee. You've got this.\n";
+  std::cout << "Jacob Wheeler, 10/5/2022\n";
+  
+  
+  
+  
+}
